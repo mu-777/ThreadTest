b8ba1688b830647a5dd98368962062a3e2fb02ea
mu-777 <ryosuke.murata.7@gmail.com>
2015-10-15 17:39:11 +0900

    update

--- main.cpp
+++ main.cpp
@@ -7,27 +7,30 @@
 
 
 // グローバル領域にmtxを用意
-std::mutex mtx;
+std::mutex mtx[4];
 
-void worker() {
+void worker(int &id) {
     // 共通の資源（ここでは標準出力）を使う前にロックをかける
-//    mtx.lock();
+    int _id = id;
+    mtx[_id].lock();
 
     // 資源を使った処理を行う
     for (int i = 0; i < 10; i++) {
-        std::cout << "thread id: " << std::this_thread::get_id() << " " << i << std::endl;
+        std::cout << _id << ": thread id: " << std::this_thread::get_id() << " " << i << std::endl;
     }
 
     // 使い終わったらロックを外す
-//    mtx.unlock();
+    mtx[_id].unlock();
 }
 
 int main() {
 
+    int i = 0;
     // スレッドを4つ作る
     std::vector<std::thread> ths(4);
     for (auto &th : ths) {
-        th = std::thread(worker);
+        th = std::thread(worker, std::ref(i));
+        i++;
     }
 
     // すべてのスレッドが終わるのを待つ
